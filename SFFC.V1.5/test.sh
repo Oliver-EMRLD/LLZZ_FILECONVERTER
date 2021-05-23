2117aac067326f4482d9dd4b995cee6f9a29b5c5
Oliver Balint <67473630+princefromplanetmoon@users.noreply.github.com>
2021-05-23 15:23:33 +0200

    Add files via upload

---
+++ SFFC.V1.5/test.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+export DD_FILEINP="/home/oliver/tst/inputfile,akurvaanyadbazdmeg"
+export DD_FILEOUT="/home/oliver/tst/out,akurvaanyadbazdmeg"
+./a.out CVT2NTHE LLZZ NO NO NO NO
+
+export DD_FILEINP="/home/oliver/tst/inputfile,akurvaanyadbazdmeg"
+export DD_FILEOUT="/home/oliver/tst/hex,akurvaanyadbazdmeg"
+./a.out CVT2NTHE LLZZ NO NO YES NO
+
+export DD_FILEINP="/home/oliver/tst/out,akurvaanyadbazdmeg"
+export DD_FILEOUT="/home/oliver/tst/outbak,akurvaanyadbazdmeg"
+./a.out CVT4NTHE FIXED NO NO NO NO
+
+export DD_FILEINP="/home/oliver/tst/hex,akurvaanyadbazdmeg"
+export DD_FILEOUT="/home/oliver/tst/hexbak,akurvaanyadbazdmeg"
+./a.out CVT4NTHE FIXED NO NO YES NO
+
+
